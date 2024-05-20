f59dae6957fc9ac88047bf1f8d61bfe371a32882
Toni Lukkaroinen <toni.lukkaroinen@hoosat.fi>
2024-05-20 20:52:50 +0300

    MiningCore pool API proxy done. Now we can have MiningCore servers in seperate servers and still have single endpoint to all of them or multiple endpoints to all of them.

---
+++ src/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 42800;
+
+// Define the base URLs of the APIs
+const endpoints = [
+    'http://fr.htn.pool.hoosat.fi',
+    'http://fr.pool.hoosat.fi'
+];
+
+// Define the endpoint to forward requests to
+const endpoint = '/data';
+
+const router = express.Router();
+
+const forwardToEndpoints = async (req, res, next) => {
+  try {
+      let responses = {};
+      console.log(req)
+      
+      for (const baseUrl of endpoints) {
+          const fullUrl = `${baseUrl}${req.originalUrl}`;
+          console.log(fullUrl);
+          const response = await fetch(fullUrl);
+          
+          if (response.ok) {
+              const data = await response.json();
+              responses = {...responses, ...data}
+          }
+      }
+
+      // Send the aggregated responses as JSON
+      res.json(responses);
+  } catch (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+// Mount the router
+app.use('/api', forwardToEndpoints);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
